bcce93a02d812596c6df5edae9f5c199807cb894
pippoji rishwanth <rishwanthpippoji@gmail.com>
2024-08-12 21:16:31 +0530

    setup file added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Medical Chatbot',
+    version= '0.0.0',
+    author= 'Rishwanth Pippoji',
+    author_email= 'rishwanthpippoji@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
